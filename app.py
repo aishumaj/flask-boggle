fb2b099dd4561600376814e0db6874f86b748b0d
Margaret Sun <hi.margaret.sun@gmail.com>
2022-07-29 15:26:29 -0700

    finish score_word()

--- app.py
+++ app.py
@@ -33,5 +33,22 @@ def new_game():
 # if not boggle.is_word_in_word_list => return "not-word"
 # if not boggle.check_word_on_board => return "not-on-board"
 # else => play_and_score_word => return "ok"
+@app.post("/api/score-word")
+def score_word():
+    breakpoint()
+    word = request.json["word"].upper()
+    game_id = request.json["gameId"]
+    current_game = games[game_id]
+
+    if not current_game.is_word_in_word_list(word):
+        return jsonify(result = "not-word")
+    elif not current_game.check_word_on_board(word):
+        return jsonify(result = "not-on-board")
+    else:
+        current_game.play_and_score_word(word)
+        return jsonify(result = "ok")
+
+
+
 
 
